3c14942eb6c36205d6b026740cf2ac07430263a2
unknown <at-for-job@yandex.ru>
2023-03-01 21:38:08 +0300

    URL getter

--- base/base_class.py
+++ base/base_class.py
@@ -2,3 +2,10 @@ class Base():
 
     def __init__(self, driver):
         self.driver = driver
+
+
+    """Метод получения текущего URL"""
+
+    def get_current_url(self):
+        get_url = self.driver.current_url
+        print(f'Текущий URL: {get_url}')
--- pages/login_page.py
+++ pages/login_page.py
@@ -57,6 +57,7 @@ class Login_page(Base):
     def authorization(self):
         self.driver.get(self.url)
         self.driver.maximize_window()
+        self.get_current_url()
         self.input_user_name(self.users["standard_user"])
         self.input_password(self.password)
         self.click_login_button()
